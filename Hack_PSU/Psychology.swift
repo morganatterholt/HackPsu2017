41344e3b56db1d599b2d19933a52968bac172a75
Shrey Arora <shreyarora2198@gmail.com>
2017-11-05 13:33:26 -0500

    final

--- Hack_PSU/Psychology.swift
+++ Hack_PSU/Psychology.swift
@@ -10,31 +10,59 @@ import UIKit
 import ARKit
 import SceneKit
 
-class Psychology: UIViewController {
+class Psychology: UIViewController, ARSCNViewDelegate {
 
-    @IBOutlet var PsychAR: ARSCNView!
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        let scene = SCNScene()
-        PsychAR.scene = scene
         
+        
+        sceneView.delegate = self
+        
+        sceneView.showsStatistics = true
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+    }
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
     }
-    */
-
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
 }
